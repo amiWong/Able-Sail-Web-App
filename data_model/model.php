9a91bcb324a0a1d069e04271e3e35eab34658017
= <alisonwong@Alisons-MacBook-Air.local>
2016-02-06 18:36:59 -0500

    Created method to check if username exists

--- data_model/model.php
+++ data_model/model.php
@@ -2,12 +2,12 @@
 
 class Database_Reader
 {
-	public $dbh;
+	private $dbh;
 
 	function __construct() {
 		try {
-      		$this->$dbh = new PDO("mysql:host=kevinzuern.com;dbname=propheis_ablesail", "propheis_able", "Ablesail");
-      		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      		$this->dbh = new PDO("mysql:host=kevinzuern.com;dbname=propheis_ablesail", "propheis_able", "Ablesail");
+      		$this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    		}
    
    		catch(PDOException $e)
@@ -16,5 +16,19 @@ class Database_Reader
    		}
 
 	}
+
+	function valid_username($username) {
+		$num_user = $this->dbh->query("SELECT * FROM `user` WHERE `username`='".$username."'");
+		$result = $num_user->fetch(PDO::FETCH_ASSOC);
+		
+		if(!$result) {
+			return FALSE;
+		}
+		else {
+			return TRUE;
+		}
+		
+	}
 }
+
 ?>
